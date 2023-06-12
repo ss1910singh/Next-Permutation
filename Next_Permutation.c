4f51993d798e10736421766cb71dac7681e5093d
ss1910singh <123332619+ss1910singh@users.noreply.github.com>
2023-06-12 19:51:41 +0530

    Add files via upload

---
+++ Next_Permutation.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void reverse(int* nums, int start, int end) {
+    while (start < end) {
+        swap(&nums[start], &nums[end]);
+        start++;
+        end--;
+    }
+}
+
+void nextPermutation(int* nums, int Size) {
+    int i = Size - 2;
+    while (i >= 0 && nums[i] >= nums[i+1]) {
+        i--;
+    }
+    if (i >= 0) {
+        int j = Size - 1;
+        while (j >= 0 && nums[i] >= nums[j]) {
+            j--;
+        }
+        swap(&nums[i], &nums[j]);
+    }
+    reverse(nums, i + 1, Size - 1);
+}
+
+int main() {
+
+    int i,size;
+    printf("Enter number of pocket:-");
+    scanf("%d",&size);
+    int nums[size];
+    printf("Enter number of chocolate in pocket:-");
+    for(i=0;i<size;i++)
+    {
+       scanf("%d",&nums[i]);  
+    }
+    nextPermutation(nums,size);
+    for (int i = 0; i < size; i++) {
+        printf("%d ", nums[i]);
+    }
+    return 0;
+}
